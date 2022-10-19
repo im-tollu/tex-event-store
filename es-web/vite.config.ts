49d61ac30ad12dfb745b8827b0596555c4f44e69
Ilja Tollu <im.tollu@gmail.com>
2022-10-20 01:07:27 +0300

    build Rescript & React with Vite

--- es-web/vite.config.ts
+++ es-web/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
+import createReactPlugin from '@vitejs/plugin-react';
+import createReScriptPlugin from '@jihchi/vite-plugin-rescript';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: []
+  plugins: [createReactPlugin(), createReScriptPlugin()]
 })
