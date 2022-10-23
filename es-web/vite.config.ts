f828a31a4c376012a89a105b1b4ad68dd97265d9
Ilja Tollu <im.tollu@gmail.com>
2022-10-23 11:26:38 +0300

    add Vitest configuration to pick up Rescript-built JS files

--- es-web/vite.config.ts
+++ es-web/vite.config.ts
@@ -1,8 +1,21 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import createReactPlugin from '@vitejs/plugin-react';
 import createReScriptPlugin from '@jihchi/vite-plugin-rescript';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createReactPlugin(), createReScriptPlugin()]
+  plugins: [createReactPlugin(), createReScriptPlugin()],
+  test: {
+    include: [
+      '**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}', // Vitest default
+      '**/*_{test,spec}.{bs.js,bs.mjs}' // built by Rescript
+    ],
+    globals: true,
+    deps: {
+      inline: ['rescript-vitest']
+    }
+  }
 })
